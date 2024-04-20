3ef1697d600aa7400c66b54e8f6189db2c9eeb35
Chininso Mba <cmba360@gmail.com>
2024-04-20 12:23:06 +0000

    commit

---
+++ models/production/monthly_sales.sql
@@ -0,0 +1,15 @@
+SELECT *
+FROM (
+    SELECT 
+        DATE_TRUNC(DATE(oi.created_at), MONTH) AS order_date,
+        SUM(oi.sale_price * o.num_of_item) AS revenue,
+        COUNT(DISTINCT oi.order_id) AS order_count,
+        COUNT(DISTINCT oi.user_id) AS customers_purchased
+    FROM `iconic-glass-418612.dbt_cmba.stgorderitems` AS oi
+    LEFT JOIN `iconic-glass-418612.dbt_cmba.stgOrders` AS o 
+        ON oi.order_id = o.order_id
+    WHERE oi.status NOT IN ('Cancelled','Returned')
+    GROUP BY 1
+    ORDER BY 1 DESC
+) AS model_limit_subq
+LIMIT 500
