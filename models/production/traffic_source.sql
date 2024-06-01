3fb4ca72d80a2aa53770c3c880b88e94fd27ba59
Chininso Mba <cmba360@gmail.com>
2024-06-01 09:06:38 +0000

    Commit

--- models/production/traffic_source.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-  u.traffic_source, 
-  COUNT(DISTINCT oi.user_id) total_customer
-FROM {{ref('stgorderitems')}} as oi
-LEFT JOIN {{ref('stgUsers')}} as u
-ON oi.user_id = u.id
-WHERE oi.status NOT IN ('Cancelled','Returned')
-GROUP BY traffic_source
-ORDER BY total_customer DESC
